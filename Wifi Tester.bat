c50429676bb9e5fa85ef27a1b03aa8cdc823fb41
Zacharyprime <ztsasser@gmail.com>
2019-07-24 14:59:26 -0500

    Now automatically connects to the Wallabies.
    You no longer need to connect to the Wallabies manually, the program creates a new profile for the Wallabies based on the serial numbers and connects itself. This is done by calculating the sha256 hash of the serial and then taking the first 6 digits of the output and adding 00 to the end (the way the wallaby password is generated).

--- Wifi Tester.bat
+++ Wifi Tester.bat
@@ -1,5 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
+:BEGIN
+cls
 color 0E
 echo -----------------------------------
 echo ^|KIPR Wallaby Wifi Testing Utility^|
@@ -12,15 +14,22 @@ for /l %%i in (1, 1, %wallabies%) do (
 )
 
 set /p tests="How many tests?: "
-set /A runtime = %tests%*5
+set /A runtime = %tests%*5+(%wallabies%*30)/60
 
 cls
-color 0c
+color 0d
 echo -----------------------------------
 echo ^|KIPR Wallaby Wifi Testing Utility^|
 echo ^|Created by: Zachary Sasser       ^|
 echo -----------------------------------
-echo The test will run %tests% times and will take approxamately %runtime% min.
+
+
+
+set /A hours=%runtime%/60
+set /A minutes=(%runtime%)%%60
+echo The test will run %tests% times and will take approxamately %hours% hours and  %minutes% minutes.
+
+
 timeout 5
 
 cls 
@@ -33,17 +42,52 @@ color 0b
 for /l %%x in (1,1,%tests%) do ( 
 	echo 	Test #%%x initiating...
 	for /l %%n in (1,1,%wallabies%) do (
-		echo Connecting to Wallaby !serial[%%n]!... 
-		netsh wlan connect name="!serial[%%n]!-wallaby" ssid="!serial[%%n]!-wallaby" 
-		if errorlevel==1 GOTO ERROR
-		timeout 10 
 		
+		for /f %%i in ('python getPassword.py !serial[%%n]!') do set PASS=%%i
+		echo The password is:!PASS!
+		timeout 5
+		echo.^<?xml version="1.0"?^> > !serial[%%n]!-wallaby.xml
+		echo.^<WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1"^> >> !serial[%%n]!-wallaby.xml
+		echo.	^<name^>!serial[%%n]!-wallaby^</name^> >> !serial[%%n]!-wallaby.xml
+		echo.	^<SSIDConfig^> >> !serial[%%n]!-wallaby.xml
+		echo.		^<SSID^> >> !serial[%%n]!-wallaby.xml
+		echo.			^<name^>!serial[%%n]!-wallaby^</name^> >> !serial[%%n]!-wallaby.xml
+		echo.		^</SSID^> >> !serial[%%n]!-wallaby.xml
+		echo.	^</SSIDConfig^> >> !serial[%%n]!-wallaby.xml
+		echo.	^<connectionType^>ESS^</connectionType^> >> !serial[%%n]!-wallaby.xml
+		echo.	^<connectionMode^>manual^</connectionMode^> >> !serial[%%n]!-wallaby.xml
+		echo.	^<MSM^> >> !serial[%%n]!-wallaby.xml
+		echo.		^<security^> >> !serial[%%n]!-wallaby.xml
+		echo.			^<authEncryption^> >> !serial[%%n]!-wallaby.xml
+		echo.				^<authentication^>WPA2PSK^</authentication^> >> !serial[%%n]!-wallaby.xml
+		echo.				^<encryption^>TKIP^</encryption^> >> !serial[%%n]!-wallaby.xml
+		echo.				^<useOneX^>false^</useOneX^> >> !serial[%%n]!-wallaby.xml
+		echo.			^</authEncryption^> >> !serial[%%n]!-wallaby.xml
+		echo.			^<sharedKey^> >> !serial[%%n]!-wallaby.xml
+		echo.				^<keyType^>passPhrase^</keyType^> >> !serial[%%n]!-wallaby.xml
+		echo.				^<protected^>false^</protected^> >> !serial[%%n]!-wallaby.xml
+		echo.				^<keyMaterial^>!PASS!^</keyMaterial^> >> !serial[%%n]!-wallaby.xml
+		echo.			^</sharedKey^> >> !serial[%%n]!-wallaby.xml
+		echo.		^</security^> >> !serial[%%n]!-wallaby.xml
+		echo.	^</MSM^> >> !serial[%%n]!-wallaby.xml
+		echo.	^<MacRandomization xmlns="http://www.microsoft.com/networking/WLAN/profile/v3"^> >> !serial[%%n]!-wallaby.xml
+		echo.		^<enableRandomization^>false^</enableRandomization^> >> !serial[%%n]!-wallaby.xml
+		echo.	^</MacRandomization^> >> !serial[%%n]!-wallaby.xml
+		echo.^</WLANProfile^> >> !serial[%%n]!-wallaby.xml
+		netsh wlan add profile filename="!serial[%%n]!-wallaby.xml"
+		
+		
+		echo. Connecting to Wallaby !serial[%%n]!... 
+		netsh wlan connect name="!serial[%%n]!-wallaby" ssid="!serial[%%n]!-wallaby" 
+		if errorlevel==1 GOTO ERROR 
 
-		echo rebooting Wallaby !serial[%%n]!... 
-		ssh root@192.168.125.1 "reboot" 
+		echo rebooting Wallaby !serial[%%n]!...
+		timeout 10
+		ssh root@192.168.125.1 "reboot"
+		echo. Wallaby !serial[%%n]! should be rebooting now. ------------------ Check for "connection reset"
+		timeout 0
 	)
-	echo Wallabies rebooted. Waiting 300 seconds for the wifi to turn on...
-
+	echo. Wallabies rebooted. Waiting for the wifi to turn on...
 	timeout 300
 	cls
 	echo -----------------------------------
@@ -61,6 +105,8 @@ pause
 EXIT \B 0
 
 :ERROR
-echo WALLABY MALFUNCTION OR ERROR HAS OCCURED
+
+color CF
+echo. WALLABY MALFUNCTION OR ERROR HAS OCCURED PLEASE TRY AGAIN
 pause
-EXIT \B 1
+GOTO BEGIN
